14d67044c3cc503b9db82fa7bdd386a95fc46f12
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2024-01-20 10:30:23 +0530

    Bug Fixed In Navigation Item

--- components/navigation/NavigationItem.tsx
+++ components/navigation/NavigationItem.tsx
@@ -29,8 +29,8 @@ const NavigationItem: React.FC<NavigationItemProps> = ({
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-            params.serverId !== id && "group-hover:h-[20px]",
-            params.serverId === id ? "h-[36px]" : "h-[8px]"
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
           )}
         />
         <div
